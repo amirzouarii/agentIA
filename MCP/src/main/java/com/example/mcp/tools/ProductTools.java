1c8064180e7f042d7fe61cdad5402b9ad7c5c6ef
amirzouarii <amir.zouari@polytechnicien.tn>
2025-12-16 13:57:17 +0100

    second

---
+++ MCP/src/main/java/com/example/mcp/tools/ProductTools.java
@@ -0,0 +1,25 @@
+package com.example.mcp.tools;
+
+import org.springaicommunity.mcp.annotation.McpTool;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ProductTools {
+
+
+    @McpTool(description = "recupere un produit à partir de son id")
+    public Produit getProduct(int id){
+        return new Produit(id, "pc portable", 5000);
+    }
+
+    @McpTool(description = "recupere la liste des produits")
+    public List<Produit> getProducts(){
+        return List.of(new Produit(1, "pc portable", 5000),
+                new Produit(2,"clavier",90),
+                new Produit(3,"ecran LED",159));
+    }
+    record Produit(int id,String nom,double prix){}
+}
