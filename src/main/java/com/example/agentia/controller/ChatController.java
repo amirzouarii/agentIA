f930a62e6d55b8e172dfdd73fc26f9fff99e96b4
amirzouarii <amir.zouari@polytechnicien.tn>
2025-12-16 14:28:01 +0100

    aa

--- src/main/java/com/example/agentia/controller/ChatController.java
+++ src/main/java/com/example/agentia/controller/ChatController.java
@@ -5,18 +5,20 @@ import com.example.agentia.tools.ProductTools;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class ChatController {
 
     private ChatClient chatClient;
-    public ChatController(ChatClient.Builder builder, ChatMemory memory, ProductTools productTools)
+    public ChatController(ChatClient.Builder builder, ChatMemory memory, SyncMcpToolCallbackProvider productTools)
     {
         this.chatClient = builder
-                .defaultTools(productTools)
+                .defaultToolCallbacks(productTools)
              //pour toute l'application   .defaultSystem("")
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(memory).build())
                 .build();
